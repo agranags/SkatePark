0289c10f858cd486ab8043bde61446c5ca715769
Arthur Graña GS <agranags@gmail.com>
2024-03-03 16:39:21 -0300

    Reorganizes the project files creating the "Art" folder and eliminating the "ThirdPerson" folder

--- Source/SkatePark/SkateParkGameMode.cpp
+++ Source/SkatePark/SkateParkGameMode.cpp
@@ -7,7 +7,7 @@
 ASkateParkGameMode::ASkateParkGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/BP_SkateParkCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
